4e4a6e71f147e4bed0591405979429852912a775
Kikiriki070204 <kiarabarrientossalazar@gmail.com>
2025-01-22 17:16:57 -0600

    cosillas

--- app.js
+++ app.js
@@ -8,7 +8,15 @@ app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
-
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/public/about.html');
+});
+app.get('/products', (req, res) => {
+  res.sendFile(__dirname + '/public/products.html');
+});
+app.get('/contact', (req, res) => {
+  res.sendFile(__dirname + '/public/contact.html');
+});
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
